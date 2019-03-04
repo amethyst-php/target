493d8182900af63600bb843b2e16e98d5b973bec
railken <railken@protonmail.ch>
2019-03-04 18:04:03 +0100

    fix config

--- config/amethyst.target.http.admin.target.php
+++ config/amethyst.target.http.admin.target.php
@@ -1,12 +1,10 @@
 <?php
 
 return [
-    'target' => [
-        'enabled'    => true,
-        'controller' => Railken\Amethyst\Http\Controllers\Admin\TargetsController::class,
-        'router'     => [
-            'as'     => 'target.',
-            'prefix' => '/targets',
-        ],
+    'enabled'    => true,
+    'controller' => Railken\Amethyst\Http\Controllers\Admin\TargetsController::class,
+    'router'     => [
+        'as'     => 'target.',
+        'prefix' => '/targets',
     ],
 ];
