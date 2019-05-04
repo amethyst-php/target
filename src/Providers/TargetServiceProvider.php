a5528c236d507133d59a2f087aa6ca2f38b778d2
railken <railken@protonmail.ch>
2019-05-04 17:33:27 +0200

    update

--- src/Providers/TargetServiceProvider.php
+++ src/Providers/TargetServiceProvider.php
@@ -13,6 +13,14 @@ class TargetServiceProvider extends CommonServiceProvider
     public function register()
     {
         parent::register();
+    }
+
+    /**
+     * @inherit
+     */
+    public function boot()
+    {
+        parent::boot();
 
         \Illuminate\Database\Eloquent\Builder::macro('targetables', function (): MorphMany {
             return app('amethyst')->createMacroMorphRelation($this, \Railken\Amethyst\Models\Targetable::class, 'targetables', 'targetable');
