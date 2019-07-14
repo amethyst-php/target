345b88e7f80c680dd104b37db81c0adf3b6e5b34
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/TargetManager.php
+++ src/Managers/TargetManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Target newEntity()
+ * @method \Amethyst\Schemas\TargetSchema getSchema()
+ * @method \Amethyst\Repositories\TargetRepository getRepository()
+ * @method \Amethyst\Serializers\TargetSerializer getSerializer()
+ * @method \Amethyst\Validators\TargetValidator getValidator()
+ * @method \Amethyst\Authorizers\TargetAuthorizer getAuthorizer()
+ */
 class TargetManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/TargetableManager.php
+++ src/Managers/TargetableManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Targetable newEntity()
+ * @method \Amethyst\Schemas\TargetableSchema getSchema()
+ * @method \Amethyst\Repositories\TargetableRepository getRepository()
+ * @method \Amethyst\Serializers\TargetableSerializer getSerializer()
+ * @method \Amethyst\Validators\TargetableValidator getValidator()
+ * @method \Amethyst\Authorizers\TargetableAuthorizer getAuthorizer()
+ */
 class TargetableManager extends Manager
 {
     use ConfigurableManager;
