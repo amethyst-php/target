4d9eb09be99077c53c12a0193c32a5ffc807cad0
railken <railken@protonmail.ch>
2019-07-14 13:12:17 +0200

    fix style

--- src/Managers/TargetManager.php
+++ src/Managers/TargetManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Target newEntity()
- * @method \Amethyst\Schemas\TargetSchema getSchema()
+ * @method \Amethyst\Models\Target                 newEntity()
+ * @method \Amethyst\Schemas\TargetSchema          getSchema()
  * @method \Amethyst\Repositories\TargetRepository getRepository()
- * @method \Amethyst\Serializers\TargetSerializer getSerializer()
- * @method \Amethyst\Validators\TargetValidator getValidator()
- * @method \Amethyst\Authorizers\TargetAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\TargetSerializer  getSerializer()
+ * @method \Amethyst\Validators\TargetValidator    getValidator()
+ * @method \Amethyst\Authorizers\TargetAuthorizer  getAuthorizer()
  */
 class TargetManager extends Manager
 {
--- src/Managers/TargetableManager.php
+++ src/Managers/TargetableManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Targetable newEntity()
- * @method \Amethyst\Schemas\TargetableSchema getSchema()
+ * @method \Amethyst\Models\Targetable                 newEntity()
+ * @method \Amethyst\Schemas\TargetableSchema          getSchema()
  * @method \Amethyst\Repositories\TargetableRepository getRepository()
- * @method \Amethyst\Serializers\TargetableSerializer getSerializer()
- * @method \Amethyst\Validators\TargetableValidator getValidator()
- * @method \Amethyst\Authorizers\TargetableAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\TargetableSerializer  getSerializer()
+ * @method \Amethyst\Validators\TargetableValidator    getValidator()
+ * @method \Amethyst\Authorizers\TargetableAuthorizer  getAuthorizer()
  */
 class TargetableManager extends Manager
 {
